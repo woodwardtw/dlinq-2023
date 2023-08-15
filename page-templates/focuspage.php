cb39d37c3e40ffc83dcb7f891793dc1e0074c84f
woodwardtw <twwoodward@me.com>
2023-08-15 09:40:48 -0400

    focus page basics

---
+++ page-templates/focuspage.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: Full Width Focus Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+if ( is_front_page() ) {
+	get_template_part( 'global-templates/hero' );
+}
+
+$wrapper_id = 'full-width-page-wrapper';
+if ( is_page_template( 'page-templates/no-title.php' ) ) {
+	$wrapper_id = 'no-title-page-wrapper';
+}
+?>
+
+<div class="wrapper" id="<?php echo $wrapper_id; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- ok. ?>">
+     <div class="major-container">
+		<div class="container">
+			<div class="major-row row">
+					<div class="dlinq-description col-md-4">
+								<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>					
+					</div>
+					<div class="dlinq-description col-md-5 offset-md-3">
+						<?php echo get_the_post_thumbnail( $post->ID, 'medium', array( 'class' => 'img-fluid aligncenter focus-pic') ); ?>
+					</div>
+			</div>
+		</div>
+    </div>
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'loop-templates/content', 'page-focus' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
+					}
+					?>
+
+				</main>
+
+			</div><!-- #primary -->
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #<?php echo $wrapper_id; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- ok. ?> -->
+
+<?php
+get_footer();
