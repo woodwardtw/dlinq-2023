d606f3c8f235ce022d2a360073166640b0913842
woodwardtw <twwoodward@me.com>
2023-08-15 13:22:57 -0400

    footer modal

--- footer.php
+++ footer.php
@@ -46,6 +46,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php wp_footer(); ?>
 
+<!--form modal-->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2 class="modal-title" id="formModalLabel">Contact DLINQ</h2>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <?php echo gravity_form( 1, true, true, false, false, true, false, false ); ?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 </body>
 
 </html>
