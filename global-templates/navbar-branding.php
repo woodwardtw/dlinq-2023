408fb720d8a17c51a8e05891a943afc532b91773
woodwardtw <twwoodward@me.com>
2023-08-24 13:30:40 -0400

    working on top menu
    cleaning up contact button ideas

--- global-templates/navbar-branding.php
+++ global-templates/navbar-branding.php
@@ -11,13 +11,13 @@ defined( 'ABSPATH' ) || exit;
 
 if ( ! has_custom_logo() ) { ?>
 
-	<?php if ( is_front_page() && is_home() ) : ?>
+	<?php if ( is_front_page() || is_home() ) : ?>
 
-		<h1 class="navbar-brand mb-0">
-			<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-				<?php bloginfo( 'name' ); ?>
+		<!-- <h1 class="navbar-brand mb-0">
+			<a rel="home" href="<?php //echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+				<?php //bloginfo( 'name' ); ?>
 			</a>
-		</h1>
+		</h1> -->
 
 	<?php else : ?>
 
