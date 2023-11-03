32b584c38aa96e0d556f915d341995b5eaaafcdc
woodwardtw <twwoodward@me.com>
2023-11-03 15:33:26 -0400

    currently routing events into gf checkbox
    probably not the way to go
    
    maybe do a value that's the id of the event for processing?
    
    way to make gf know which form and field to process this way . . . need to loop through and make form entries for each event

--- functions.php
+++ functions.php
@@ -658,7 +658,47 @@ function dlinq_attendance_update() {
     // wp_send_json_error([/* some data here */]);
 }
 
+//provide future events as a drop down in the bulk enrollment form
 
+add_filter( 'gform_pre_render_6', 'dlinq_populate_events' );
+add_filter( 'gform_pre_validation_6', 'dlinq_populate_events' );
+add_filter( 'gform_pre_submission_filter_6', 'dlinq_populate_events' );
+add_filter( 'gform_admin_pre_render_6', 'dlinq_populate_events' );
+function dlinq_populate_events( $form ) {
+ 
+    foreach( $form['fields'] as &$field )  {
+ 
+        //NOTE: replace 3 with your checkbox field id
+        $field_id = 5;
+        if ( $field->id != $field_id ) {
+            continue;
+        }
+ 
+        // you can add additional parameters here to alter the posts that are retreieved
+        // more info: http://codex.wordpress.org/Template_Tags/get_posts
+        $posts = get_posts( 'numberposts=-1&post_status=publish&post_type=tribe_events' );
+ 
+        $input_id = 1;
+        foreach( $posts as $post ) {
+ 
+            //skipping index that are multiples of 10 (multiples of 10 create problems as the input IDs)
+            if ( $input_id % 10 == 0 ) {
+                $input_id++;
+            }
+ 
+            $choices[] = array( 'text' => $post->post_title, 'value' => $post->post_title );
+            $inputs[] = array( 'label' => $post->post_title, 'id' => "{$field_id}.{$input_id}" );
+ 
+            $input_id++;
+        }
+ 
+        $field->choices = $choices;
+        $field->inputs = $inputs;
+ 
+    }
+ 
+    return $form;
+}
 
 
 //LOGGER -- like frogger but more useful
