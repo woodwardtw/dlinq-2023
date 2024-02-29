5de7436332bc23f5e1a69eb4c67ddb0b921ef5db
woodwardtw <twwoodward@me.com>
2024-02-29 11:58:23 -0500

    fix type on cpts dropdown for workshops

--- functions.php
+++ functions.php
@@ -518,7 +518,7 @@ function dlinq_workshop_resources(){
 
 
 //add workshops dropdown to workshop form
-$gf_workshop_request_id = get_field('workshop_registration_form', 'option');
+$gf_workshop_request_id = get_field('workshop_request_form', 'option');
 $pre_render = 'gform_pre_render_' . $gf_workshop_request_id;
 add_filter( 'gform_pre_render_' . $gf_workshop_request_id , 'populate_cpt_titles' );
 add_filter( 'gform_pre_validation_' . $gf_workshop_request_id,'populate_cpt_titles' );
@@ -526,7 +526,6 @@ add_filter( 'gform_pre_submission_filter_' . $gf_workshop_request_id, 'populate_
 add_filter( 'gform_admin_pre_render_' . $gf_workshop_request_id, 'populate_cpt_titles' );
 function populate_cpt_titles( $form ) {
 	
-
 	foreach ( $form['fields'] as &$field ) {
 		if ( $field->id != 7 ) {
 	    		continue;
