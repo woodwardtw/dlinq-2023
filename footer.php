405d0f4e3f762a43e8d1e82cd2b9bd9512c4a81e
woodwardtw <twwoodward@me.com>
2024-03-04 11:23:19 -0500

    footer

--- footer.php
+++ footer.php
@@ -32,6 +32,43 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<?php the_field('footer_right','option');?>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer-right', 'container_class' => 'footer-menu' ) ); ?>
 				</div>
+				<div class="col-lg-3">
+
+          <a href="https://www.middlebury.edu">
+            <img src="https://www.middlebury.edu/themes/custom/middlebury_theme/images/middlebury-logo-white.svg" alt="Middlebury" width="195" height="71">
+          </a>
+
+      </div>
+      <div class="col-lg-9">
+
+        <ul class="midd-footer__list">
+          <li class="midd-footer__item">
+            <a href="https://www.middlebury.edu/about" class="midd-footer__link">About Middlebury</a>
+          </li>
+          <li class="midd-footer__item">
+            <a href="https://www.middlebury.edu/alumni-and-families" class="midd-footer__link">Giving</a>
+          </li>
+          <li class="midd-footer__item">
+            <a href="https://www.middlebury.edu/human-resources/work-middlebury" class="midd-footer__link">Employment</a>
+          </li>
+          <li class="midd-footer__item">
+            <a href="https://www.middlebury.edu/offices-services" class="midd-footer__link">Offices and Services</a>
+          </li>
+          <li class="midd-footer__item">
+            <a href="https://www.middlebury.edu/about/copyright" class="midd-footer__link">Copyright</a>
+          </li>
+          <li class="midd-footer__item">
+            <a href="https://www.middlebury.edu/about/website-privacy-policy" class="midd-footer__link">Privacy</a>
+          </li>
+          <li class="midd-footer__item">
+            <a href="https://www.middlebury.edu/emergency-response" class="midd-footer__link">Emergency</a>
+          </li>
+          <li class="midd-footer__item">
+              <a href="/saml/login?destination=/node/17521" class="midd-footer__link" rel="nofollow">Site-Editor Login</a>
+          </li>
+        </ul>
+
+      </div>
 
 			<div class="col-md-12">
 
