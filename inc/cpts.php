d4c5369f6b5195194f4c945b2627b8b4842efd2b
woodwardtw <twwoodward@me.com>
2024-07-22 15:13:24 -0400

    add resource cpt
    change dashicon for quote

--- inc/cpts.php
+++ inc/cpts.php
@@ -265,7 +265,7 @@ function create_quote_cpt() {
     'show_in_rest' => true,
     'publicly_queryable' => true,
     'capability_type' => 'post',
-    'menu_icon' => 'dashicons-universal-access-alt',
+    'menu_icon' => 'dashicons-editor-quote',
   );
   register_post_type( 'quote', $args );
   
@@ -340,3 +340,70 @@ function create_topic_cpt() {
   $wp_rewrite->flush_rules();
 }
 add_action( 'init', 'create_topic_cpt', 0 );
+
+
+//resource custom post type
+
+// Register Custom Post Type resource
+// Post Type Key: resource
+
+function create_resource_cpt() {
+
+  $labels = array(
+    'name' => __( 'Resources', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Resource', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Resource', 'textdomain' ),
+    'name_admin_bar' => __( 'Resource', 'textdomain' ),
+    'archives' => __( 'Resource Archives', 'textdomain' ),
+    'attributes' => __( 'Resource Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Resource:', 'textdomain' ),
+    'all_items' => __( 'All Resources', 'textdomain' ),
+    'add_new_item' => __( 'Add New Resource', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Resource', 'textdomain' ),
+    'edit_item' => __( 'Edit Resource', 'textdomain' ),
+    'update_item' => __( 'Update Resource', 'textdomain' ),
+    'view_item' => __( 'View Resource', 'textdomain' ),
+    'view_items' => __( 'View Resources', 'textdomain' ),
+    'search_items' => __( 'Search Resources', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into resource', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this resource', 'textdomain' ),
+    'items_list' => __( 'Resource list', 'textdomain' ),
+    'items_list_navigation' => __( 'Resource list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Resource list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'resource', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-admin-links',
+  );
+  register_post_type( 'resource', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_resource_cpt', 0 );
