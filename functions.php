533e2eebc04c04f33cdbe624001dabc3c6621ea3
woodwardtw <twwoodward@me.com>
2024-09-30 13:43:38 -0400

    fix getting form id instead of field id
    which just happened to match in testing

--- functions.php
+++ functions.php
@@ -943,7 +943,7 @@ function after_submission_bulk_enroll( $entry, $form ) {
  	$first = rgar($entry, '1.3');
  	$last = rgar($entry, '1.6');
  	$email = rgar($entry, '3');
- 	$events = rs_gf_get_checked_boxes( $entry,  $gf_workshop_registration_id );
+ 	$events = rs_gf_get_checked_boxes( $entry,  5 );
  	write_log($events);
  	foreach ($events as $key => $event_id) {
  		$event_id = intval($event_id);
