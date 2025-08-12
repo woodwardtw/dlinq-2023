dfc619647b9b824966f0ba78c0a4feac6e54b6e2
Tom Woodward <twwoodward@me.com>
2025-08-12 08:53:31 -0400

    single workshop email issue

--- functions.php
+++ functions.php
@@ -762,7 +762,8 @@ function dlinq_registered_people($form_id){
 				$created = substr($result["date_created"], 0, 10);//get rid of the time
 				$first = $result["1.3"];
 				$last = $result["1.6"];
-				$email = $result["4"];
+				$email = $result["3"];
+				var_dump($result);
 				$attendance = $result["8"];
 				$attendance_count = ($attendance === 'Yes') ? intval($attendance_count)+1 : $attendance_count;
 				$attendance_options = [
