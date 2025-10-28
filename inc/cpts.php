3294e092bd6d708856bc0ca4c835619af274ed42
Tom Woodward <twwoodward@me.com>
2025-10-28 14:11:55 -0400

    start prompt library cpt and acf changes

--- inc/cpts.php
+++ inc/cpts.php
@@ -541,3 +541,41 @@ function create_definition_cpt() {
   $wp_rewrite->flush_rules();
 }
 add_action( 'init', 'create_definition_cpt', 0 );
+
+
+function create_prompt_cpt() {
+
+    $labels = array(
+      'name' => __( 'Prompts', 'Post Type General Name', 'textdomain' ),
+      'singular_name' => __( 'Prompt', 'Post Type Singular Name', 'textdomain' ),
+       'add_new_item' => __( 'Add New Definition', 'textdomain' ),
+      'add_new' => __( 'Add New', 'textdomain' ),
+      'new_item' => __( 'New Prompt', 'textdomain' ),
+      'edit_item' => __( 'Edit Prompt', 'textdomain' ),
+    );
+    $args = array(
+      'label' => __( 'prompt', 'textdomain' ),
+      'description' => __( '', 'textdomain' ),
+      'labels' => $labels,
+      'public' => true,
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'menu_position' => 5,
+      'show_in_admin_bar' => true,
+      'show_in_nav_menus' => true,
+      'can_export' => true,
+      'has_archive' => true,
+      'hierarchical' => false,
+      'exclude_from_search' => false,
+      'show_in_rest' => true,
+      'publicly_queryable' => true,
+      'capability_type' => 'post',
+      'menu_icon' => 'dashicons-welcome-write-blog',
+    );
+    register_post_type( 'prompt', $args );
+    
+    // flush rewrite rules because we changed the permalink structure
+    global $wp_rewrite;
+    $wp_rewrite->flush_rules();
+  }
+  add_action( 'init', 'create_prompt_cpt', 0 );
